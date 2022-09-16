806289a49d4820bbc9948f169ec46af4a44dacab
Annarchive <ytpt@mail.ru>
2022-09-16 15:18:24 +0400

    Small refactor in ButtonList.jsx

--- src/Components/MainScreen/Buttons/ButtonsList.jsx
+++ src/Components/MainScreen/Buttons/ButtonsList.jsx
@@ -11,14 +11,14 @@ const ButtonsList = () => {
     }
 
     return (
-        <ul className={s.buttonsBlock}>
+        <div className={s.buttonsBlock}>
             <button key={1} id={'1'} onClick={handleClick} className={isActive === '1'
                 ? classNames(s.active,s.button) : classNames(s.button)}>Now</button>
             <button key={2} id={'2'} onClick={handleClick} className={isActive === '2'
                 ? classNames(s.active,s.button) : classNames(s.button)}>Details</button>
             <button key={3} id={'3'} onClick={handleClick} className={isActive === '3'
                 ? classNames(s.active,s.button) : classNames(s.button)}>Forecast</button>
-        </ul>
+        </div>
     )
 }
 
