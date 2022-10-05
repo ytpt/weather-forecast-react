d5eda38409a0512be14ea0f1daaee253ac37a950
Annarchive <ytpt@mail.ru>
2022-10-05 23:57:51 +0800

    Added fetchWeatherData to get weather when clicking at city from favorite list in City.jsx

--- src/Components/MainScreen/FavoriteList/City.jsx
+++ src/Components/MainScreen/FavoriteList/City.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import {ImCross} from 'react-icons/im';
 import s from './FavoriteCity.module.css';
+import {getWeather} from "../../../api";
+import {updateMainCityAC} from "../../../redux/reducers/mainCity-reducer";
 
-const City = ({city, deleteFromFavList}) => {
+const City = ({setWeather, city, deleteFromFavList, dispatch}) => {
+
+    const fetchWeatherData = async () => {
+        setWeather(await getWeather(city.name));
+        localStorage.setItem('mainCity', city.name);
+        dispatch(updateMainCityAC(city.name));
+    }
 
     return (
         <li className={s.favCity}>
-            <a className={s.favCityLink} onClick={localStorage.setItem('favCity', city.name)}>
+            <a className={s.favCityLink} onClick={fetchWeatherData}>
                 <p>{city.name}</p>
             </a>
             <button onClick={() => deleteFromFavList(city.id)}>
