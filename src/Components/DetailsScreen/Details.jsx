770828cd37f1cb48d14317b458d5d0462edc1843
Annarchive <ytpt@mail.ru>
2022-10-10 16:08:13 +0800

    - ASmall refactoring in Details.jsx

--- src/Components/DetailsScreen/Details.jsx
+++ src/Components/DetailsScreen/Details.jsx
@@ -6,13 +6,13 @@ const Details = ({weather, currentDay}) => {
     const date = new Date(weather.dt * 1000);
     currentDay(date);
 
-        const sunriseDate = new Date(weather.sunrise * 1000);
-        const sunrise = ((sunriseDate.getHours() < 10 ? '0' : '') + sunriseDate.getHours()) + ':' + // Время в формате 00:00
-            ((sunriseDate.getMinutes() < 10 ? '0' : '') + sunriseDate.getMinutes());
+    const sunriseDate = new Date(weather.sunrise * 1000);
+    const sunrise = ((sunriseDate.getHours() < 10 ? '0' : '') + sunriseDate.getHours()) + ':' + // Время в формате 00:00
+        ((sunriseDate.getMinutes() < 10 ? '0' : '') + sunriseDate.getMinutes());
 
-        const sunsetDate = new Date(weather.sunset * 1000);
-        const sunset = ((sunsetDate.getHours()< 10 ? '0' : '') + sunsetDate.getHours()) + ':' + // Время в формате 00:00
-            ((sunsetDate.getMinutes()< 10 ? '0' : '') + sunsetDate.getMinutes());
+    const sunsetDate = new Date(weather.sunset * 1000);
+    const sunset = ((sunsetDate.getHours()< 10 ? '0' : '') + sunsetDate.getHours()) + ':' + // Время в формате 00:00
+        ((sunsetDate.getMinutes()< 10 ? '0' : '') + sunsetDate.getMinutes());
 
 
     return (
