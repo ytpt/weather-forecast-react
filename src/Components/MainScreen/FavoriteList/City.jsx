f40b5963f27283ee83c23c599bf6186bfb980910
Annarchive <ytpt@mail.ru>
2022-10-10 15:59:20 +0800

    - Added isFav and setIsFav props to City component in City.jsx; - Added useEffect hook to control the isFav state and color it; - Added deleteFromFavList function to delete city from favorite list and control the isFav state;

--- src/Components/MainScreen/FavoriteList/City.jsx
+++ src/Components/MainScreen/FavoriteList/City.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {ImCross} from 'react-icons/im';
 import s from './FavoriteCity.module.css';
 import {getWeather} from "../../../api";
 import {updateMainCityAC} from "../../../redux/reducers/mainCity-reducer";
+import {deleteFromFavoriteAC} from "../../../redux/reducers/favorite-reducer";
 
-const City = ({setWeather, city, deleteFromFavList, dispatch}) => {
+const City = ({ setWeather, city, dispatch, isFav, setIsFav}) => {
+
+    useEffect(() => {
+        if (isFav === false) {
+            document.querySelector('#likeIcon').style.color = 'black';
+        }
+    }, [isFav]);
 
     const fetchWeatherData = async () => {
         setWeather(await getWeather(city.name));
@@ -12,12 +19,21 @@ const City = ({setWeather, city, deleteFromFavList, dispatch}) => {
         dispatch(updateMainCityAC(city.name));
     }
 
+    const deleteFromFavList = (cityName) => {
+        dispatch(deleteFromFavoriteAC(cityName));
+
+        const mainCity = localStorage.getItem('mainCity');
+        if (mainCity === cityName) {
+            setIsFav(false);
+        }
+    }
+
     return (
         <li className={s.favCity}>
             <a className={s.favCityLink} onClick={fetchWeatherData}>
                 <p>{city.name}</p>
             </a>
-            <button onClick={() => deleteFromFavList(city.id)}>
+            <button onClick={() => deleteFromFavList(city.name)}>
                 <ImCross className={s.favCity_icon} />
             </button>
         </li>
